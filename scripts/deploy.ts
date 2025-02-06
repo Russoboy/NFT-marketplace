040320efad2147b27143a7b265e11be25fe6eedf
Daniel Adigun <105540876+Russoboy@users.noreply.github.com>
2025-02-06 15:32:02 +0100

    Create deploy.ts
    Deployment & Test Scripts in TypeScript 💻

---
+++ scripts/deploy.ts
@@ -0,0 +1,18 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const MyNFT = await ethers.getContractFactory("MyNFT");
+  const myNFT = await MyNFT.deploy();
+  await myNFT.deployed();
+  console.log("MyNFT deployed to:", myNFT.address);
+
+  const Marketplace = await ethers.getContractFactory("Marketplace");
+  const marketplace = await Marketplace.deploy();
+  await marketplace.deployed();
+  console.log("Marketplace deployed to:", marketplace.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
